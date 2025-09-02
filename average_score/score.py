66d18e15dae85d3771b76feb838cc59cde0de4a0
taegyu jang <taegyu1500@gmail.com>
2025-09-02 17:53:11 +0900

    문제풀이

--- average_score/score.py
+++ average_score/score.py
@@ -27,7 +27,8 @@ def subject_average(student_scores: dict, subjects: list):
     이 반의 각 과목별 평균을 구해서 딕셔너리로 반환
     예) {"국어": 80.8, "수학": 35.3, "영어": 96.6, "과학": 85.3, "사회": 38.8}
     """
-    pass
+    return dict(zip(subjects, [sum(map(int, subject_scores)) / len(subject_scores) for subject_scores in zip(*student_scores.values())]))
+
 
 
 def student_average(student_scores: dict):
@@ -35,7 +36,7 @@ def student_average(student_scores: dict):
     각 학생별 전과목 평균 점수를 정렬된 튜플의 리스트로 반환
     예) [("이영희", 89.8), ("김철수", 86.6), ("박민수", 84.8)]
     """
-    pass
+    return sorted(((name, sum(map(int, scores)) / len(scores)) for name, scores in student_scores.items()), key=lambda x: x[1], reverse=True)
 
 
 if __name__ == "__main__":
@@ -47,10 +48,10 @@ if __name__ == "__main__":
     sub_avg = subject_average(student_scores, subjects)
     stud_avg = student_average(student_scores)
 
-    print("과목 평균:")
-    for sub, avg in sub_avg.items():
-        print(f"\t{sub}: {avg:.2f}")
+    # print("과목 평균:")
+    # for sub, avg in sub_avg.items():
+    #     print(f"\t{sub}: {avg:.2f}")
 
-    print("학생 점수:")
-    for avg in stud_avg:
-        print(f"\t{avg[0]}: {avg[1]:.2f}")
+    # print("학생 점수:")
+    # for avg in stud_avg:
+    #     print(f"\t{avg[0]}: {avg[1]:.2f}")
